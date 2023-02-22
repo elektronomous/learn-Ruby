0133e2a0dc765239d3921ee989ef5ad6c7f42a25
Muhammad Faza Akbar <akbarfaza222@gmail.com>
2023-02-23 00:14:44 +0800

    methods section one

---
+++ VIDEOS/Section2/methods/4_p.rb
@@ -0,0 +1,17 @@
+puts "Muhammad Faza Akbar using puts method";
+p "Muhammad Faza Akbar using p method";         # p tell use this is a string => "Muhammad Faza Akbar using p method"
+# the p give us the output of some detail about the output
+
+puts "5";       # => 5
+puts 5;         # => 5
+# puts doesn't care what the output is, it just output to the screen
+# now using the p method
+p "5";          # => "5"
+p 5;            # 5
+
+# p tells you what object is being displayed
+puts "After this text\nis on the new line.";    # After this text
+                                                # is on the new line.
+p "After this text\nis on the new line.";       # => "After this text\nis on the new line."
+# p help you debug, to display some detail about what you're going to do.
+# puts and print recommend to use when we encounter for the user sake.
